bd0e03b4df59844f62648cba584b0db227d4d5a7
Rico Groth <ricogstudy@gmail.com>
2022-10-16 21:45:07 +0200

    Added Rational::gcd() and trim functionality

--- Uebung1/src/main/scala/uebung1/App.scala
+++ Uebung1/src/main/scala/uebung1/App.scala
@@ -5,8 +5,8 @@ object App {
   def main(args: Array[String]): Unit = {
     
     println("Hier startet das erste Scala Programm!")
-    val x = new Rational(1, 4)
+    val x = new Rational(-1, -4)
     val y = new Rational(2, 4)
-    println(x.sub(y))
+    println(x)
   }
 }
